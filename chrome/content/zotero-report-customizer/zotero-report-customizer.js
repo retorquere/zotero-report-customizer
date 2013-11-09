4ffe2fadf3fc853a72da80e1157f7d45bd3aa3ca
friflaj <friflaj@ajellito.com>
2013-11-09 12:42:17 +0100

    Multi-language capable, prepping for config screen

--- chrome/content/zotero-report-customizer/zotero-report-customizer.js
+++ chrome/content/zotero-report-customizer/zotero-report-customizer.js
@@ -1,5 +1,45 @@
 Zotero.ReportCustomizer = {
+  discardableFields: [],
+  deleteFields: [],
+  removeTags: null,
+  removeAttachments: null,
+
 	init: function () {
+    for (field of ['abstractNote', 'accessDate', 'applicationNumber', 'archive',
+                    'archiveLocation', 'artworkMedium', 'artworkSize', 'assignee',
+                    'attachments', 'audioFileType', 'audioRecordingFormat', 'billNumber',
+                    'blogTitle', 'bookTitle', 'callNumber', 'caseName', 'code',
+                    'codeNumber', 'codePages', 'codeVolume', 'committee', 'company',
+                    'conferenceName', 'country', 'court', 'date', 'dateAdded',
+                    'dateDecided', 'dateEnacted', 'dateModified', 'dictionaryTitle',
+                    'distributor', 'docketNumber', 'documentNumber', 'DOI', 'edition',
+                    'encyclopediaTitle', 'episodeNumber', 'extra', 'filingDate',
+                    'firstPage', 'forumTitle', 'genre', 'history', 'institution',
+                    'interviewMedium', 'ISBN', 'ISSN', 'issue', 'issueDate',
+                    'issuingAuthority', 'journalAbbreviation', 'label', 'language',
+                    'legalStatus', 'legislativeBody', 'letterType', 'libraryCatalog',
+                    'manuscriptType', 'mapType', 'medium', 'meetingName', 'nameOfAct',
+                    'network', 'notes', 'number', 'numberOfVolumes', 'numPages',
+                    'pages', 'patentNumber', 'place', 'postType', 'presentationType',
+                    'priorityNumbers', 'proceedingsTitle', 'programmingLanguage',
+                    'programTitle', 'publicationTitle', 'publicLawNumber', 'publisher',
+                    'references', 'related', 'reporter', 'reporterVolume', 'reportNumber',
+                    'reportType', 'rights', 'runningTime', 'scale', 'section', 'series',
+                    'seriesNumber', 'seriesText', 'seriesTitle', 'session', 'shortTitle',
+                    'source', 'studio', 'subject', 'system', 'tags', 'thesisType', 'title',
+                    'university', 'url', 'version', 'videoRecordingFormat', 'volume',
+                    'websiteTitle', 'websiteType']) {
+      Zotero.ReportCustomizer.discardableFields[field] = Zotero.getString('itemFields.' + field);
+    }
+
+    Zotero.ReportCustomizer.deleteFields = ['callNumber', 'ISBN', 'ISSN', 'issue', 'journalAbbreviation',
+                                            'libraryCatalog', 'numPages', 'pages', 'place', 'publicationTitle',
+                                            'publisher', 'series', 'seriesNumber', 'shortTitle', 'thesisType',
+                                            'volume', 'language', 'url', 'rights', 'websiteType', 'accessDate',
+                                            'dateAdded', 'dateModified', 'DOI'];
+    Zotero.ReportCustomizer.removeTags = true;
+    Zotero.ReportCustomizer.removeAttachments = true;
+
     // monkey-patch Zotero.Report.generateHTMLDetails to modify the generated report
     Zotero.Report.generateHTMLDetails = (function (self, original) {
       return function (items, combineChildItems) {
@@ -7,55 +47,37 @@ Zotero.ReportCustomizer = {
         var report = parser.parseFromString(original.apply(this, arguments), 'text/html');
 
         console.log('Scrubbing report');
+        var unlinkNodes = [];
 
         try {
-          var toUnlink = [];
+          var unlinkRows = [Zotero.ReportCustomizer.discardableFields[field] for (field of Zotero.ReportCustomizer.deleteFields)];
+          unlinkRows = [text for (text of unlinkRows) if ((typeof text) != 'undefined' && text != null && text != '')];
 
           var node;
           var nodes
           
           nodes = report.evaluate('//th', report, null, XPathResult.ANY_TYPE, null );
           while (node = nodes.iterateNext()) {
-            switch (node.textContent) {
-              case 'Call Number':
-              case 'ISBN':
-              case 'ISSN':
-              case 'Issue':
-              case 'Journal Abbr':
-              case 'Library Catalog':
-              case '# of Pages':
-              case 'Pages':
-              case 'Place':
-              case 'Publication':
-              case 'Publisher':
-              case 'Series':
-              case 'Series Number':
-              case 'Short Title':
-              case 'Type':
-              case 'Volume':
-              case 'Language':
-              case 'URL':
-              case 'Rights':
-              case 'Accessed':
-              case 'Website Type':
-              case 'Date Added':
-              case 'Modified':
-              case 'DOI':
-                toUnlink.push(node.parentNode);
+            if (unlinkRows.indexOf(node.textContent) > -1) {
+              unlinkNodes.push(node.parentNode);
             }
           }
         } catch (err) {
           console.log('Scrub failed: ' + err);
         }
 
-        nodes = report.evaluate("//ul[@class='attachments']", report, null, XPathResult.ANY_TYPE, null );
-        while (node = nodes.iterateNext()) { toUnlink.push(node); }
-        nodes = report.evaluate("//h3[@class='tags']", report, null, XPathResult.ANY_TYPE, null );
-        while (node = nodes.iterateNext()) { toUnlink.push(node); }
-        nodes = report.evaluate("//ul[@class='tags']", report, null, XPathResult.ANY_TYPE, null );
-        while (node = nodes.iterateNext()) { toUnlink.push(node); }
+        if (Zotero.ReportCustomizer.removeAttachments) {
+          nodes = report.evaluate("//ul[@class='attachments']", report, null, XPathResult.ANY_TYPE, null );
+          while (node = nodes.iterateNext()) { unlinkNodes.push(node); }
+        }
+        if (Zotero.ReportCustomizer.removeTags) {
+          nodes = report.evaluate("//h3[@class='tags']", report, null, XPathResult.ANY_TYPE, null );
+          while (node = nodes.iterateNext()) { unlinkNodes.push(node); }
+          nodes = report.evaluate("//ul[@class='tags']", report, null, XPathResult.ANY_TYPE, null );
+          while (node = nodes.iterateNext()) { unlinkNodes.push(node); }
+        }
         
-        for (node of toUnlink) {
+        for (node of unlinkNodes) {
           try {
             node.parentNode.removeChild(node);
           } catch (err) {
@@ -65,7 +87,6 @@ Zotero.ReportCustomizer = {
         return ser.serializeToString(report);
       }
     })(this, Zotero.Report.generateHTMLDetails);
-
 	}
 };
 
