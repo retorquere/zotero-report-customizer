1ddf90215d0d7de9ccb9d38d7ca3e4737e6449ba
friflaj <friflaj@ajellito.com>
2013-12-06 22:12:18 +0100

    0.0.9

--- chrome/content/zotero-report-customizer/options.js
+++ chrome/content/zotero-report-customizer/options.js
@@ -5,7 +5,7 @@ function setLabels() {
   var field, label, id, checkbox;
   for (field of Object.keys(Zotero.ReportCustomizer.discardableFields)) {
     id = 'id-zotero-report-customizer_remove_' + field;
-    label = z_stringsBundle.getString('itemFields.' + field);
+    label = z_stringsBundle.getString('itemFields.' + field) + ' (' + field + ')';
     console.log('report: ' + field + ': ' + label);
     checkbox = document.getElementById(id);
     checkbox.setAttribute('label', label);
