7beffbc02477890b95f54192474c1526359e0b53
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2018-02-21 19:47:55 +0100

    ts

---
+++ content/ReportCustomizer.ts
@@ -0,0 +1,77 @@
+declare const Zotero: any
+
+const ReportCustomizer = new class { // tslint:disable-line:variable-name
+  private template: string
+
+  constructor() {
+    window.addEventListener('load', e => { this.load() }, false)
+  }
+
+  public get() {
+    try {
+      this.template = Zotero.Prefs.get('report-customizer.template')
+    } catch (err) {
+      this.log('failed to load template', err)
+      this.template = ''
+    }
+
+    if (!this.template) {
+      this.template = Zotero.File.getContentsFromURL('resource://zotero-report-customizer/template.html')
+    }
+    return this.template
+  }
+
+  public set(template) {
+    this.template = template
+    return Zotero.Prefs.set('report-customizer.template', template)
+  }
+
+  public log(...msg) {
+    const result = []
+    for (const m of msg) {
+      if (typeof m === 'number' || typeof m === 'string' || m instanceof String) result.push(`${m}`)
+
+      else if (Array.isArray(m)) result.push(JSON.stringify(m))
+
+      else if (typeof m === 'object' && m.name && m.fileName) result.push(`${m.name}: ${m.message} \n(${m.fileName}, ${m.lineNumber})\n${m.stack}`)
+
+      else if (m instanceof Error) result.push(`${m}\n${m.stack}`)
+
+      else result.push(JSON.stringify(m))
+    }
+    Zotero.debug(`[report-customizer] ${result.join(' ')}`)
+  }
+
+  private async load() {
+    this.log('starting')
+  }
+}
+
+function install_url_handler(resource) {
+  Zotero.Server.Endpoints[`/report-customizer/${resource}`] = class {
+    public supportedMethods = ['GET']
+    public OK = 200
+    public SERVER_ERROR = 500
+
+    public async init(request) {
+      try {
+        return [200, 'text/plain', Zotero.File.getContentsFromURL(`resource://zotero-report-customizer/${resource}`)] // tslint:disable-line:no-magic-numbers
+      } catch (err) {
+        ReportCustomizer.log(`could not serve URL ${resource}`, err)
+        return [500, 'application/text', `RC failed: ${err}\n${err.stack}`] // tslint:disable-line:no-magic-numbers
+      }
+    }
+  }
+}
+for (const resource of [
+  'index.html',
+  'zotero-z-32px-australis-unix.svg',
+  'token/lang/en.js',
+  'token/lang/ru.js',
+  'token/plugin.js',
+  'token/dialogs/token.js',
+]) {
+  install_url_handler(resource)
+}
+
+export = ReportCustomizer
---
+++ webpack.config.ts
@@ -0,0 +1,53 @@
+// tslint:disable:no-console
+
+import * as webpack from 'webpack'
+import * as path from 'path'
+
+import BailPlugin from 'zotero-plugin/plugin/bail'
+
+import CircularDependencyPlugin = require('circular-dependency-plugin')
+
+import 'zotero-plugin/make-dirs'
+import 'zotero-plugin/copy-assets'
+import 'zotero-plugin/rdf'
+import 'zotero-plugin/version'
+
+const config = {
+  node: { fs: 'empty' },
+  resolveLoader: {
+    alias: {
+      'json-loader': path.join(__dirname, './zotero-webpack/loader/json.ts'),
+      'wrap-loader': 'zotero-plugin/loader/wrap',
+    },
+  },
+  module: {
+    rules: [
+      { test: /\.json$/, use: [ 'json-loader' ] },
+      { test: /\.ts$/, exclude: [ /node_modules/ ], use: [ 'wrap-loader', 'ts-loader' ] },
+    ],
+  },
+
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new CircularDependencyPlugin({ failOnError: true }),
+    BailPlugin,
+  ],
+
+  context: path.resolve(__dirname, './content'),
+
+  entry: {
+    ReportCustomizer: './ReportCustomizer.ts',
+  },
+
+  output: {
+    path: path.resolve(__dirname, './build/content'),
+    filename: '[name].js',
+    jsonpFunction: 'Zotero.WebPackedReportCustomizer',
+    devtoolLineToLine: true,
+    pathinfo: true,
+    library: 'Zotero.[name]',
+    libraryTarget: 'assign',
+  },
+}
+
+export default config
