a2ff983591570c01d7050164033ca34f905178aa
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-01-25 13:15:48 +0100

    remove works

--- content/report.ts
+++ content/report.ts
@@ -1,5 +1,30 @@
 // tslint:disable:no-console
 
-function reorder(stuff) {
-  console.log(stuff)
+const state = {
+  editing: false,
+  remove: [],
+}
+
+function toggleEdit() {
+  state.editing = !state.editing
+
+  for (const x of document.getElementsByClassName('delete-field')) {
+    (x as HTMLElement).style.display = state.editing ? 'inline-block' : 'none'
+  }
+
+  return false
+}
+
+function deleteField(type) {
+  if (state.remove.indexOf(type) < 0) state.remove.push(type)
+  update()
+  return false
+}
+
+function update() {
+  for (const cls of state.remove) {
+    for (const field of document.getElementsByClassName(cls)) {
+      (field as HTMLElement).style.display = 'none'
+    }
+  }
 }
