7d315dc6b80ca5ecb80f328763081d98ec500938
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-01-29 09:54:26 +0100

    oo

--- content/report.ts
+++ content/report.ts
@@ -1,19 +1,6 @@
 declare const config: ReportConfig
 declare const defaults: ReportConfig
 
-const state: { editing: boolean, saved: boolean, config: ReportConfig } = {
-  editing: false,
-  saved: false,
-
-  config: JSON.parse(JSON.stringify(config)),
-}
-
-function _log(msg) {
-  return
-  const pre = document.getElementById('log')
-  pre.textContent += `${msg}\n`
-}
-
 function equal(a, b) {
   const ta = typeof a
   const tb = typeof b
@@ -51,94 +38,115 @@ function equal(a, b) {
   return true
 }
 
-function isDirty() {
-  return !equal(config, state.config)
-}
+const report = new class {
+  public saved: boolean
 
-window.onbeforeunload = function(e) { // tslint:disable-line:only-arrow-functions
-  return !state.saved && isDirty() ? true : undefined
-}
+  private config: ReportConfig
+  private editing: boolean
 
-function toggleEdit() {
-  _log('toggleEdit')
-  state.editing = !state.editing
+  constructor() {
+    this.editing = false
+    this.saved = false
 
-  for (const x of document.getElementsByClassName('delete-field')) {
-    (x as HTMLElement).style.display = state.editing ? 'inline-block' : 'none'
+    this.config = JSON.parse(JSON.stringify(config))
+    this.update()
   }
 
-  return false
-}
+  public dirty() {
+    return !equal(config, this.config)
+  }
 
-function deleteField(field) {
-  _log('deleteField')
-  if (state.config.fields.remove.indexOf(field.dataset.type) < 0) state.config.fields.remove.push(field.dataset.type)
-  update()
-  return false
-}
+  public toggleEdit() {
+    this.log('toggleEdit')
+    this.editing = !this.editing
 
-function restore() {
-  _log('restore')
-  state.config = JSON.parse(JSON.stringify(config))
-  update()
-  return false
-}
+    for (const x of document.getElementsByClassName('delete-field')) {
+      (x as HTMLElement).style.display = this.editing ? 'inline-block' : 'none'
+    }
 
-function reset() {
-  _log('reset')
-  state.config = JSON.parse(JSON.stringify(defaults))
-  update()
-  return false
-}
+    return false
+  }
 
-function save() {
-  const backend = (document.getElementById('backend') as HTMLIFrameElement).contentWindow
-  _log('save: dirty=' + isDirty() + ', backend=' + !!backend) // tslint:disable-line:prefer-template
-  if (isDirty()) {
+  public deleteField(field) {
+    this.log('deleteField')
+    if (this.config.fields.remove.indexOf(field.dataset.type) < 0) this.config.fields.remove.push(field.dataset.type)
+    this.update()
+    return false
+  }
+
+  public restore() {
+    this.log('restore')
+    this.config = JSON.parse(JSON.stringify(config))
+    this.update()
+    return false
+  }
+
+  public reset() {
+    this.log('reset')
+    this.config = JSON.parse(JSON.stringify(defaults))
+    this.update()
+    return false
+  }
+
+  public save() {
+    if (!this.dirty()) return false
+
+    const backend = (document.getElementById('backend') as HTMLIFrameElement).contentWindow
     if (backend) {
-      backend.postMessage(JSON.stringify(state.config), '*')
+      backend.postMessage(JSON.stringify(this.config), '*')
     } else {
       alert(`backend not available: ${!!document.getElementById('backend')}`)
     }
+
+    return false
   }
-  return false
+
+  public log(msg) {
+    return
+    const pre = document.getElementById('log')
+    pre.textContent += `${msg}\n`
+  }
+
+  private update() {
+    this.log('update; dirty=' + this.dirty())
+
+    document.getElementById('save').style.display = this.dirty() ? 'inline-block' : 'none'
+    document.getElementById('undo').style.display = !equal(this.config, config) ? 'inline-block' : 'none'
+    document.getElementById('reset').style.display = !equal(this.config, defaults) && !equal(config, defaults) ? 'inline-block' : 'none'
+
+    const show: {[key: string]: string} = {}
+
+    for (const field of document.querySelectorAll('[data-type]')) {
+      const type = (field as HTMLElement).dataset.type
+      show[type] = ''
+    }
+
+    for (const type of this.config.fields.remove) {
+      show[type] = 'none'
+    }
+
+    for (const [type, display] of Object.entries(show)) {
+      for (const field of document.getElementsByClassName(type)) {
+        (field as HTMLElement).style.display = display
+      }
+    }
+  }
 }
+
+window.onbeforeunload = function(e) { // tslint:disable-line:only-arrow-functions
+  return report.dirty() ? true : undefined
+}
+
 window.onmessage = function(e) { // tslint:disable-line:only-arrow-functions
   // e.data can be 'saved' or 'error'
-  _log('message: got ' + e.data)
+  report.log('message: got ' + e.data)
 
   // this will prevent the onbeforeunload complaining
-  state.saved = true
+  window.onbeforeunload = undefined
 
   // this will reload the report and thereby get the latest saved state
   location.reload(true)
 }
 
-function update() {
-  _log('update; dirty=' + isDirty())
-
-  document.getElementById('save').style.display = isDirty() ? 'inline-block' : 'none'
-  document.getElementById('undo').style.display = !equal(state.config, config) ? 'inline-block' : 'none'
-  document.getElementById('reset').style.display = !equal(state.config, defaults) ? 'inline-block' : 'none'
-
-  const show: {[key: string]: string} = {}
-
-  for (const field of document.querySelectorAll('[data-type]')) {
-    const type = (field as HTMLElement).dataset.type
-    show[type] = ''
-  }
-
-  for (const type of state.config.fields.remove) {
-    show[type] = 'none'
-  }
-
-  for (const [type, display] of Object.entries(show)) {
-    for (const field of document.getElementsByClassName(type)) {
-      (field as HTMLElement).style.display = display
-    }
-  }
-}
-
 // onload does not seem to fire within Zotero
-_log('report loaded')
-update()
+report.log('report loaded')
