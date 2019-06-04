70da84a27fb601769732c8863ac3161a311fc10f
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-06-04 23:30:11 +0200

    sort attachments

--- content/zotero-report-customizer.ts
+++ content/zotero-report-customizer.ts
@@ -145,6 +145,10 @@ function* listGenerator(items, combineChildItems) {
       }
     }
 
+    if (item.attachments) {
+      item.attachments.sort((a, b) => (a.title || '').localeCompare(b.title || '', undefined, { sensitivity: 'base' }))
+    }
+
     // quality report
     const qualityReport = []
     const nonSpaceWhiteSpace = /[\u00A0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u200B\u202F\u205F\u3000\uFEFF]/
